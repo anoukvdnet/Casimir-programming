158455668bdcea40caa2dfc9aaac5e7fc43738bd
Anouk van der Net <J.J.P.vanderNet@tudelft.nl>
2022-01-31 12:02:22 +0100

    my first commit

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print('Hello World')
+
+import math
+
+radius = float(input("Enter the radius of the circle : "))
+
+circumference = 2 * math.pi * radius
+
+print("Circumference of the circle is : %.2f" % circumference)
